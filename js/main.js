6164e207a2e713abf3436928c6f153b056a33846
ochen1997 <och21003@byui.edu>
2023-02-12 22:40:49 -0700

    Html created

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const API_KEY = ""
+document.addEventListener("DOMContentLoaded", init);
+function init(){
+    document.querySelector("#button").addEventListener("click",output)
+    const search = document.querySelector("#search").value;
+}
+
+function output(giphys){
+    
+}
+
+async function getGiphys(){
+    const Url = ""
+    const response = await fetch(Url)
+}
